f9f8273a5ca06164684a460e7f8332329ae8457b
ecopsych <schoenjack98@gmail.com>
2024-02-18 14:19:46 -0500

    create repo

---
+++ app.py
@@ -0,0 +1,32 @@
+import os
+from flask import Flask, render_template, send_from_directory
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('update_content')
+def handle_update_content(message):
+    socketio.emit('update', message)
+
+@app.route('/tabs')
+def list_tabs():
+    tabs = os.listdir('tabs/')
+    return render_template('list_tabs.html', tabs=tabs)
+
+@app.route('/tabs/<filename>')
+def tab_file(filename):
+    return send_from_directory('tabs', filename)
+
+@app.route('/send_update/<message>')
+def send_update(message):
+    socketio.emit('update', {'message': message})
+    return "Message sent: " + message
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=5000)
