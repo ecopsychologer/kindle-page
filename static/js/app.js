9c6823cfa40db756aa914da3d79403eb50c39c58
JC <jschoen@duck.com>
2024-03-13 10:31:20 -0400

    more fixes

--- static/js/app.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
-
-    socket.on('update', function(data) {
-        document.getElementById('content').innerHTML = data.message;
-    });
-});
